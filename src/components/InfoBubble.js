fdb40f138ecb24f26a570b877873abeaaa0a91b3
MattRuetz <matt.b.ruetz@gmail.com>
2023-03-15 13:53:58 -0400

    initial, header started

---
+++ src/components/InfoBubble.js
@@ -0,0 +1,5 @@
+const InfoBubble = () => {
+    return <div></div>;
+};
+
+export default InfoBubble;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <>
+            <section id="hero" className="section"></section>
+            <section id="info" className="section">
+                INFO SECTION
+            </section>
+        </>
+    );
+};
+
+export default Home;
