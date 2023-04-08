e006da62e96313f113cac4f19909916dca283f6a
mruetz <mruetz@uoguelph.ca>
2023-04-08 12:49:28 -0400

    stages off timeline, home passes single timeline to roadmap.. better performance

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -1,145 +1,138 @@
-import { useEffect, useState } from 'react';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import gsap from 'gsap';
-import Stages from './Stages';
+import { useEffect, useState } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from "gsap";
+import Stages from "./Stages";
 
 gsap.registerPlugin(ScrollTrigger);
 
 // Stages is an array of strings, each representing a stage
-const Roadmap = ({ stages }) => {
-    const [showIndex, setShowIndex] = useState(0);
-    const [hideIndex, setHideIndex] = useState(-1);
+const Roadmap = ({ stages, timeline }) => {
+  const [showIndex, setShowIndex] = useState(0);
+  const [hideIndex, setHideIndex] = useState(-1);
 
-    useEffect(() => {
-        const roadmapSection = document.getElementById('roadmap');
-        roadmapSection.style.setProperty('height', `${100 * stages.length}vh`);
+  useEffect(() => {
+    const roadmapSection = document.getElementById("roadmap");
+    roadmapSection.style.setProperty("height", `${100 * stages.length}vh`);
 
-        const tl2 = gsap.timeline();
-        // create context. This function is invoked immediately and all GSAP animations and ScrollTriggers created during the execution of this function get recorded so we can revert() them later (cleanup)
+    // create context. This function is invoked immediately and all GSAP animations and ScrollTriggers created during the execution of this function get recorded so we can revert() them later (cleanup)
 
-        // Cache Selectors
-        const barContainer = document.querySelector('.bar-container');
-        const content = document.querySelector('.content-area');
-        const fillItems = document.querySelectorAll('.animate-fill');
+    // Cache Selectors
+    const barContainer = document.querySelector(".bar-container");
+    const content = document.querySelector(".content-area");
+    const fillItems = document.querySelectorAll(".animate-fill");
 
-        // Define callback functions
-        function showContent() {
-            content.classList.remove('hidden');
-        }
-        function hideContent() {
-            content.classList.add('hidden');
-        }
-        function incrementStage(index) {
-            setShowIndex(index + 1);
-            setHideIndex(index);
-        }
-        function decrementStage(index) {
-            setShowIndex(index);
-            setHideIndex(index + 1);
-        }
+    // Define callback functions
+    function showContent() {
+      content.classList.remove("hidden");
+    }
+    function hideContent() {
+      content.classList.add("hidden");
+    }
+    function incrementStage(index) {
+      setShowIndex(index + 1);
+      setHideIndex(index);
+    }
+    function decrementStage(index) {
+      setShowIndex(index);
+      setHideIndex(index + 1);
+    }
 
-        let ctx = gsap.context(() => {
-            tl2.to(barContainer, {
-                scrollTrigger: {
-                    trigger: barContainer,
-                    start: 'top top',
-                    end: 'bottom bottom',
-                    pin: true,
-                },
-            })
-                .from('.seg-0', {
-                    scaleY: 0,
-                    scrollTrigger: {
-                        trigger: '.seg-0',
-                        start: 'top bottom',
-                        end: 'top top',
-                        scrub: 2,
-                    },
-                })
-                // Planning slides up when entering roadmap section
-                .from(content, {
-                    y: 1000,
-                    scrollTrigger: {
-                        trigger: roadmapSection,
-                        start: 'top center',
-                        end: `+=${window.innerHeight * 0.5}`,
-                        scrub: 1,
-                        markers: true,
-                        onEnter: showContent,
-                        onLeaveBack: hideContent,
-                    },
-                })
-                //
-                .to(content, {
-                    scrollTrigger: {
-                        trigger: roadmapSection,
-                        start: 'bottom bottom',
-                        end: `+=${window.innerHeight}`,
-                        scrub: 1,
-                        markers: true,
-                        onLeave: hideContent,
-                        onEnterBack: showContent,
-                    },
-                });
-            // animates progress bar filling between stages
-            fillItems.forEach((fillItem, index) =>
-                tl2.from(fillItem, {
-                    scaleY: 0,
-                    transformOrigin: 'left top',
-                    scrollTrigger: {
-                        trigger: roadmapSection,
-                        start: `${(index + 1) * window.innerHeight} center`,
-                        end: `+=${window.innerHeight * 0.1}`,
-                        // markers: true,
-                        scrub: 2,
-                        onLeave: () => incrementStage(index),
-                        onLeaveBack: () => decrementStage(index),
-                    },
-                })
-            );
+    let ctx = gsap.context(() => {
+      timeline
+        .to(barContainer, {
+          scrollTrigger: {
+            trigger: barContainer,
+            start: "top top",
+            end: "bottom bottom",
+            pin: true,
+          },
+        })
+        .from(".seg-0", {
+          scaleY: 0,
+          scrollTrigger: {
+            trigger: ".seg-0",
+            start: "top bottom",
+            end: "top top",
+            scrub: 2,
+          },
+        })
+        // Planning slides up when entering roadmap section
+        .from(content, {
+          y: 1000,
+          scrollTrigger: {
+            trigger: roadmapSection,
+            start: "top center",
+            end: `+=${window.innerHeight * 0.5}`,
+            scrub: 1,
+            markers: true,
+            onEnter: showContent,
+            onLeaveBack: hideContent,
+          },
+        })
+        //
+        .to(content, {
+          scrollTrigger: {
+            trigger: roadmapSection,
+            start: "bottom bottom",
+            end: `+=${window.innerHeight}`,
+            scrub: 1,
+            markers: true,
+            onLeave: hideContent,
+            onEnterBack: showContent,
+          },
         });
+      // animates progress bar filling between stages
+      fillItems.forEach((fillItem, index) =>
+        timeline.from(fillItem, {
+          scaleY: 0,
+          transformOrigin: "left top",
+          scrollTrigger: {
+            trigger: roadmapSection,
+            start: `${(index + 1) * window.innerHeight} center`,
+            end: `+=${window.innerHeight * 0.1}`,
+            // markers: true,
+            scrub: 2,
+            onLeave: () => incrementStage(index),
+            onLeaveBack: () => decrementStage(index),
+          },
+        })
+      );
+    });
 
-        return () => ctx.revert(); // cleanup
-    }, [stages]);
+    return () => ctx.revert(); // cleanup
+  }, [stages]);
 
-    return (
-        <>
-            <div id="bar-container" className="bar-container">
-                <div className="bar-blank"></div>
-                <div className="bar-filled seg-0"></div>
+  return (
+    <>
+      <div id='bar-container' className='bar-container'>
+        <div className='bar-blank'></div>
+        <div className='bar-filled seg-0'></div>
 
-                <div className="nodes">
-                    {stages.map((stage, index) => (
-                        <div
-                            className="node-item"
-                            style={{
-                                paddingTop:
-                                    ((index * 100) / (stages.length * 100)) *
-                                        100 +
-                                    7 +
-                                    'vh',
-                            }}
-                        >
-                            <div
-                                className={`bar-filled animate-fill seg-${
-                                    index + 1
-                                } total-seg-${stages.length}`}
-                            ></div>
-                            <div className="node indicator inactive"></div>
-                            <div className="node text">{stage}</div>
-                        </div>
-                    ))}
-                </div>
+        <div className='nodes'>
+          {stages.map((stage, index) => (
+            <div
+              className='node-item'
+              style={{
+                paddingTop:
+                  ((index * 100) / (stages.length * 100)) * 100 + 7 + "vh",
+              }}
+            >
+              <div
+                className={`bar-filled animate-fill seg-${
+                  index + 1
+                } total-seg-${stages.length}`}
+              ></div>
+              <div className='node indicator inactive'></div>
+              <div className='node text'>{stage}</div>
             </div>
-            <div className="content-area hidden">
-                <Stages
-                    showIndex={showIndex}
-                    hideIndex={hideIndex}
-                    stages={stages}
-                />
-            </div>
-        </>
-    );
+          ))}
+        </div>
+      </div>
+      <div className='content-area hidden'>
+        <Stages showIndex={showIndex} hideIndex={hideIndex} stages={stages} />
+      </div>
+    </>
+  );
 };
 
 export default Roadmap;
--- src/components/Stages.js
+++ src/components/Stages.js
@@ -1,52 +1,49 @@
-import { useEffect } from 'react';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import gsap from 'gsap';
+import { useEffect } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from "gsap";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Stages = ({ showIndex, hideIndex, stages }) => {
-    useEffect(() => {
-        let hideElement = document.querySelector(`.content-ind-${hideIndex}`);
-        let showElement = document.querySelector(`.content-ind-${showIndex}`);
+  useEffect(() => {
+    let hideElement = document.querySelector(`.content-ind-${hideIndex}`);
+    let showElement = document.querySelector(`.content-ind-${showIndex}`);
 
-        const tl3 = gsap.timeline();
+    let ctx = gsap.context(() => {
+      gsap.to(hideElement, {
+        yPercent: showIndex > hideIndex ? "150" : "-150",
+        opacity: 0,
+        duration: 0.1,
+      });
+      gsap.from(showElement, {
+        yPercent: showIndex > hideIndex ? "-150" : "150",
+        opacity: 0,
+        duration: 0.5,
+        onStart: () => {
+          showElement && showElement.classList.remove("hidden");
+          document.querySelectorAll(".stage-content").forEach((item, i) => {
+            if (i !== showIndex) {
+              item.classList.add("hidden");
+              console.log("hiding " + i + " " + showIndex);
+            }
+          });
+        },
+      });
+    });
+    return () => ctx.revert(); // cleanup
+  }, [showIndex, hideIndex]);
 
-        let ctx = gsap.context(() => {
-            tl3.to(hideElement, {
-                yPercent: showIndex > hideIndex ? '150' : '-150',
-                opacity: 0,
-                duration: 0.1,
-            }).from(showElement, {
-                yPercent: showIndex > hideIndex ? '-150' : '150',
-                opacity: 0,
-                duration: 0.5,
-                onStart: () => {
-                    showElement && showElement.classList.remove('hidden');
-                    document
-                        .querySelectorAll('.stage-content')
-                        .forEach((item, i) => {
-                            if (i !== showIndex) {
-                                item.classList.add('hidden');
-                                console.log('hiding ' + i + ' ' + showIndex);
-                            }
-                        });
-                },
-            });
-        });
-        return () => ctx.revert(); // cleanup
-    }, [showIndex, hideIndex]);
-
-    return (
-        <>
-            {stages.map((stage, index) => {
-                return (
-                    <div className={`stage-content content-ind-${index}`}>
-                        <h1 className="stage-heading">{stage}</h1>
-                    </div>
-                );
-            })}
-        </>
-    );
+  return (
+    <>
+      {stages.map((stage, index) => {
+        return (
+          <div className={`stage-content content-ind-${index}`}>
+            <h1 className='stage-heading'>{stage}</h1>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Stages;
